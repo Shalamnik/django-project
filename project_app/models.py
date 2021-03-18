a8da1940834c209fd4f2168aee6bd65a153e786f
n.shalamberidze <shalamberidze00@bk.ru>
2021-03-18 15:14:12 +0300

    Create the superuser "admin" and Topic to the admin site

--- project_app/models.py
+++ project_app/models.py
@@ -8,3 +8,16 @@ class Topic(models.Model):
 
     def __str__(self):
         return self.text
+
+
+class Entry(models.Model):
+    """Something specific learned about a topic."""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return a string representation of the model"""
+        return f"{self.text[:50]}..."
